4c7f0e5526f62ea3642e4b35ffe272441103df1b
Kevin Chi <kevinchi118@gmail.com>
2018-08-06 20:10:44 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -31,6 +31,7 @@ class PetsController < ApplicationController
 
   post '/pets/:id' do
     @pet = Pet.find(params[:id])
+    if Owner.find(params[:pet])
     @pet.update(params[:pet])
     if !params[:owner_name].empty?
       @pet.owner = Owner.create(name: params[:owner_name])
